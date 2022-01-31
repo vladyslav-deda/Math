434344b0d2ba1d0371050f1244a863d089152f26
Vlad070707 <51382570+Vlad070707@users.noreply.github.com>
2022-02-01 00:26:38 +0200

    1001 - Show Dialog on start of main page (#3)

--- app/src/main/java/com/example/mathwithfinik/ui/mainscreen/FinikFirstSpeachDialog.kt
+++ app/src/main/java/com/example/mathwithfinik/ui/mainscreen/FinikFirstSpeachDialog.kt
@@ -1,20 +1,27 @@
 package com.example.mathwithfinik.ui.mainscreen
 
-import android.app.Activity
 import android.app.Dialog
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.view.Window
+import android.widget.Button
 import com.example.mathwithfinik.R
 
-object FinikFirstSpeachDialog  {
+object FinikFirstSpeachDialog {
 
-    fun showDialog(context: Activity) {
-        val dialog = Dialog(context)
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setCancelable(false);
-        dialog.setContentView(R.layout.finik_first_speach_dialog_layout)
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    fun showDialog(context: Context?) {
+        val dialog = context?.let { Dialog(it) }
+        dialog?.let { dialog ->
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setCancelable(false);
+            dialog.setContentView(R.layout.finik_first_speach_dialog_layout)
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialog.findViewById<Button>(R.id.speach_dialog_ok_button).setOnClickListener {
+                dialog.dismiss()
+            }
+            dialog.show()
+        }
     }
+
 }
